2006206ac6937559d277845284d19a149f2e6a59
DarreTheKing <97673555+DarreTheKing@users.noreply.github.com>
2022-12-07 13:36:24 +0100

    Projekt

---
+++ Assets/C# Scripts/StartScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    public string boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] boss = new string[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
